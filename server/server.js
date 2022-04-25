f263095f3ea90fc429cf631e88b3b16caa49c7ab
John David Delgado <johndavid@Johns-MacBook-Pro.local>
2022-04-24 22:04:41 -0400

    server configuration update for live deployment

--- server/server.js
+++ server/server.js
@@ -1,20 +1,42 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
-const mongoose = require('mongoose');
 const expressjwt = require('express-jwt');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
 const PORT = process.env.PORT || 9000;
 
 app.use(express.json());
 app.use(morgan('dev'));
 
+const allowedDomains = [
+  'https://johndemployment-tracker.netlify.app',
+  'http://localhost:3000',
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedDomains.indexOf(origin) === -1) {
+        const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 app.use('/auth', require('./routes/authRouter.js'));
-app.use('/api', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] }));
+app.use(
+  '/api',
+  expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] })
+);
 app.use('/api/issues', require('./routes/issuesRouter.js'));
 app.use('/api/issues/comments', require('./routes/commentRouter.js'));
 
 mongoose.connect(
-  'mongodb://localhost:27017/rvt',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/rvt',
   console.log('connected to db')
 );
 
